6c3e2a10df7e3abb9265bf11e9c2e69d32798248
Nacho <imoyared@gmail.com>
2023-01-28 12:23:47 +0100

    Write summary dataframe to csv and reads all years of available data

--- main.py
+++ main.py
@@ -1,48 +1,142 @@
 import os
-import pandas as pd
+import re
+import json
 import pyspark
-from pyspark.sql.types import DataType, BooleanType, NullType, IntegerType, StringType, MapType
-from pyspark.sql.functions import udf, explode
+from pyspark.sql.functions import explode
+from json.decoder import JSONDecodeError
 
-def get_dirs():
-    """ Returns a list of directory paths where financial json files are stored """
-    DIR = 'archive'
+
+def ini_spark():
+    "Initializes spark session"
+    spark = (
+        pyspark.sql.SparkSession.builder.appName("Decode_json_files")
+        .config("spark.jars")
+        .getOrCreate()
+    )
+    return spark
+
+
+def get_dirs(directory="archive"):
+    "Returns a list of directory paths where financial json files are stored"
     dir_list = []
-    dir_list += [os.path.join(DIR,file) for file in os.listdir(DIR) if os.path.isdir(os.path.join(DIR, file))]
+    dir_list += [
+        os.path.join(directory, file)
+        for file in os.listdir(directory)
+        if os.path.isdir(os.path.join(directory, file))
+    ]
     dir_list.sort()
     return dir_list
 
+
 def get_json_in_folder(folder):
-    """ Returns a list of jsons files which are in a particular folder"""
+    "Returns a list of jsons files which are in a particular folder"
     json_files = []
     json_files += [os.path.join(folder, file) for file in os.listdir(folder)]
     return json_files
 
-def main():
-    """
-    1. Get directories where json files are located and chooses one quarter 
-    2. JSON file is selected from the available ones
-    3. Initialises Spark session and reads JSON file selected
-    4. Defines a dataframe where Balance Sheet is stored
-    
-    """
-    dir_list = get_dirs()
-    # Get latest quarter
-    quarter_folder = dir_list[-2]
-    json_files = get_json_in_folder(quarter_folder)
-    # Get first stock of the json files
-    json_file = json_files[0]
-    # Spark session
-    spark = pyspark.sql.SparkSession.builder \
-        .appName("Decode_json_files") \
-        .config("spark.some.config.option", "some-value") \
-        .getOrCreate()
-    df = spark.read.option("multiline", "true").json(json_file)
-    df_bs = df.withColumn("bs", explode('data.bs')).select('bs')
+
+def get_year_qtr_of_dir(filedir):
+    "Returns year and quarter of a given filedir"
+    year = int(re.search("archive/(.*).QTR", filedir).group(1))
+    qtr = int(re.search("QTR(.*)", filedir).group(1))
+    return year, qtr
+
+
+def read_bs(df):
+    "Reads balance sheet from spark dataframe object"
+    df_bs = df.withColumn("bs", explode("data.bs")).select("bs")
+    df_bs = df_bs.select("bs.concept", "bs.label", "bs.unit", "bs.value").distinct()
     return df_bs
 
+
+def read_cf(df):
+    "Reads cash flow from spark dataframe object"
+    df_cf = df.withColumn("bs", explode("data.cf")).select("cf")
+    df_cf = df_cf.select("cf.concept", "cf.label", "cf.unit", "cf.value").distinct()
+    return df_cf
+
+
+def read_ic(df):
+    "Reads income statement from spark dataframe object"
+    df_ic = df.withColumn("bs", explode("data.cf")).select("ic")
+    df_ic = df_ic.select("ic.concept", "ic.label", "ic.unit", "ic.value").distinct()
+    return df_ic
+
+
+def read_ticker_in_json(json_file):
+    "Returns spark dataframe containing ticker symbol from json file"
+    try:
+        df = spark.read.option("multiline", "true").json(json_file)
+        return df.select("symbol").collect()[0][0]
+    except NameError:
+        print('Please initialize Spark session in variable called "spark"')
+
+
+def get_year_qtr_of_dir(filedir):
+    year = int(re.search("archive/(.*).QTR", filedir).group(1))
+    qtr = int(re.search("QTR(.*)", filedir).group(1))
+    return year, qtr
+
+
+def main():
+    df = spark.read.option("multiline", "true").json(json_file)
+    return df
+
+
+def get_ticker_in_json(json_file):
+    try:
+        with open(json_file, "r") as jf:
+            df = json.load(jf)
+        return df["symbol"]
+    except (IsADirectoryError, JSONDecodeError):
+        pass
+
+
+def dump_summary():
+    dir_list = get_dirs()
+    spark = ini_spark()
+    for quarter_folder in dir_list:
+        json_files = get_json_in_folder(quarter_folder)
+        collect_df = []
+        for json_id in range(len(json_files)):
+            json_file = json_files[json_id]
+            ticker = get_ticker_in_json(json_file)
+            year, quarter = get_year_qtr_of_dir(quarter_folder)
+            collect_df.append(
+                {"json_id": json_id, "ticker": ticker, "year": year, "quarter": quarter}
+            )
+        df = spark.createDataFrame(collect_df)
+        print(df.show())
+        df.write.csv(f"data/summary_df_{year}_{quarter}.csv")
+
+
+def read_summary():
+    dir_list = get_dirs()
+    spark = ini_spark()
+    df = None
+    for quarter_folder in dir_list:
+        year, quarter = get_year_qtr_of_dir(quarter_folder)
+        print(f"data/summary_df_{year}_{quarter}.csv")
+
+        if df is None:
+            df = spark.read.csv(f"data/summary_df_{year}_{quarter}.csv")
+            # print(df.show())
+        else:
+            tmp_df = spark.read.csv(f"data/summary_df_{year}_{quarter}.csv")
+            df = df.union(tmp_df)
+    return df
+
+
 if __name__ == "__main__":
-    main()
-    
-    
-    
+    dump_sum = False
+    if dump_sum == True:
+        dump_summary()
+    else:
+        pass
+    df = read_summary()
+    print(df.show())
+
+    # parquet_files = get_dirs(directory='data')
+    # spark = ini_spark()
+    # mergedDF = spark.read.option("mergeSchema", "true").parquet(parquet_files[0])
+    # print(mergedDF.count())
